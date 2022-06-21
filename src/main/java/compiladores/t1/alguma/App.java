c66f65e2059247145bc1972c05d70223c92d281a
GabrielDeJesus <gabriel.jdantas1@gmail.com>
2022-06-21 16:44:25 -0300

    Documentation

--- src/main/java/compiladores/t1/alguma/App.java
+++ src/main/java/compiladores/t1/alguma/App.java
@@ -19,35 +19,35 @@ public class App
 
         try(PrintWriter p = new PrintWriter(new FileWriter(args[1]))) {//saida
             CharStream c = CharStreams.fromFileName(args[0]);//entrada
-            alguma lex = new alguma(c);
+            alguma lex = new alguma(c); //passa o charstream lido do arquivo para o "compilador" identificar o token
 
             Token t = null;
-            while((t = lex.nextToken()).getType() != Token.EOF){
-                String text = '\'' + t.getText() + '\'';
-                String aType = alguma.VOCABULARY.getDisplayName(t.getType());
-                // System.out.println("ON " + text + " we have " + aType);
-                if(aType == "Nao_Fechado"){
+            while((t = lex.nextToken()).getType() != Token.EOF){ //pega o proximo token enquanto não encontrar o fim do arquivo
+                String text = '\'' + t.getText() + '\''; //converte o texto do token lido para o formato entre ''
+                String aType = alguma.VOCABULARY.getDisplayName(t.getType()); // Converte o tipo desse token para string
+                
+                if(aType == "Nao_Fechado"){ //Caso seja comentario nao fechado
                     p.println("Linha " + t.getLine() + ": " + "comentario nao fechado");
                     break;
                 }
-                else if(aType == "Literal_Nao_Fechada"){
+                else if(aType == "Literal_Nao_Fechada"){ //Caso seja literal "" nao fechado
                     p.println("Linha " + t.getLine() + ": " + "cadeia literal nao fechada");
                     break;
                 }
-                else if(aType == "ERR"){
+                else if(aType == "ERR"){ //Em caso de Simbolo nao identificado
                     p.println("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                     break;
                 }
                 else if(aType == "IDENT" || aType == "CADEIA" || aType == "NUM_INT" || aType == "NUM_REAL"){
+                    // caso de tokens/ tipos definidos
                     p.println("<"+ text + "," + aType + ">");
                 }
-                else
+                else //caso geral de palavras reservadas, cujo nome do token é o mesmo que a string lida
                     p.println("<"+ text + "," + text + ">");
             }
 
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            //TODO: handle exception
         }
     }
 }
